386a2e11d04adccda94ef270799797c6c4843052
Lakshya Kapoor <lakshya.kapoor@ns8.com>
2020-07-14 20:35:51 -0700

    Add test for launcher.

--- test/launcher.test.js
+++ test/launcher.test.js
@@ -1,27 +1,42 @@
 import AwsDeviceFarmLauncher from '../src/launcher'
-import Utils from "../src/utils";
+import utils from "../src/utils";
 
 describe('AwsDeviceFarmLauncher', () => {
-    beforeEach(() => {
-        const getTestGridInfo = jest
-            .fn()
-            .mockReturnValueOnce({
-                hostname: 'test-host',
-                path: 'test-path',
-                port: '1234',
-                protocol: 'https'
-            })
-    })
+    describe('onPrepare', () => {
+        test('should set the correct config', async () => {
+            // Test config
+            const config = {
+                services: [
+                    [
+                        "aws-device-farm",
+                        {
+                            region: "us-west-2", // Only one available as of April 2020
+                            accessKeyId: 'test-access-key',
+                            secretAccessKey: 'test-secret-key',
+                            projectArn: 'test-project-arn',
+                            expiresInSeconds: 300, // 5 minutes
+                        },
+                    ],
+                ]
+            }
 
-    test('sets the hostname', () => {
-    })
+            // Mock getTestGridInfo
+            utils.getTestGridInfo = jest.fn().mockReturnValue(
+                    {
+                        hostname: 'test-grid-host',
+                        path: '/test-grid-path',
+                        port: '1234',
+                        protocol: 'https'
+                    }
+            )
 
-    test('sets the path', () => {
-    })
+            let launcher = new AwsDeviceFarmLauncher()
+            await launcher.onPrepare(config)
 
-    test('sets the port', () => {
-    })
-
-    test('sets the protocol', () => {
+            expect(config.hostname).toBe('test-grid-host')
+            expect(config.path).toBe('/test-grid-path')
+            expect(config.port).toBe('1234')
+            expect(config.protocol).toBe('https')
+        })
     })
 })
